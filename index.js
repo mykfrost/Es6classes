0a79f98f15754a27e8ccc7762002f3a4acdeeb62
Michael Okello <okellofrost@gmail.com>
2022-07-19 13:20:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Old classes
+
+// function Circle(radius){
+
+//     this.radius = radius;
+
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+// }
+
+class Circle{
+
+}
